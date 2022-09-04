bf9d6ce98f111a4bf182269d653f57010aaad5a2
rewritestar <rewritestar@naver.com>
2022-09-04 17:13:00 +0900

    버튼 클릭 함수들 구현

--- calculator/src/app.jsx
+++ calculator/src/app.jsx
@@ -6,47 +6,82 @@ function App() {
   const [input, setInput] = useState("");
   const onNumberClick = (e) => {
     const number = e.target.innerText;
-    let newInput = input;
-    newInput += number;
+    const newInput = input + number;
     setInput(newInput);
   };
-  const onReset = (e) => {
+  const onResetClick = (e) => {
     setInput("");
+  };
+  const onDeleteClick = (e) => {
+    const newInput = input.slice(0, -1);
+    setInput(newInput);
+  };
+  const onOperatorClick = (e) => {
+    const operator = e.target.innerText;
+    const prev = input.slice(-1);
+    //연산자가 중첩되지 않도록 만듦
+    if (
+      prev === "/" ||
+      prev === "x" ||
+      prev === "-" ||
+      prev === "+" ||
+      prev === "%"
+    ) {
+      return;
+    }
+    const newInput = input + operator;
+    setInput(newInput);
+  };
+  const onEqualClick = (e) => {
+    const prev = input.slice(-1);
+    if (
+      prev === "/" ||
+      prev === "x" ||
+      prev === "-" ||
+      prev === "+" ||
+      prev === "%"
+    ) {
+      alert("완성되지 않은 수식입니다.");
+    }
+    operation();
+  };
+  const operation = (e) => {
+    console.log(input);
   };
   return (
     <div className={styles.app}>
       <div className={styles.container}>
-        <input className={styles.show} type="text" value={input} />
+        <input className={styles.show} type="text" defaultValue={input} />
         <section className={styles.buttons}>
           <div className={styles.buttons_container}>
-            <Button name="C" colorType="operator" onClick={onReset} />
-            <Button name="<=" colorType="operator" onClick={onReset} />
-            <Button name="%" colorType="operator" onClick={onReset} />
-            <Button name="/" colorType="operator" onClick={onReset} />
+            <Button name="C" colorType="operator" onClick={onResetClick} />
+            <Button name="<=" colorType="operator" onClick={onDeleteClick} />
+            <Button name="%" colorType="operator" onClick={onOperatorClick} />
+            <Button name="/" colorType="operator" onClick={onOperatorClick} />
           </div>
           <div className={styles.buttons_container}>
             <Button name="7" colorType="number" onClick={onNumberClick} />
             <Button name="8" colorType="number" onClick={onNumberClick} />
             <Button name="9" colorType="number" onClick={onNumberClick} />
-            <Button name="x" colorType="operator" onClick={onReset} />
+            <Button name="x" colorType="operator" onClick={onOperatorClick} />
           </div>
           <div className={styles.buttons_container}>
             <Button name="4" colorType="number" onClick={onNumberClick} />
             <Button name="5" colorType="number" onClick={onNumberClick} />
             <Button name="6" colorType="number" onClick={onNumberClick} />
-            <Button name="-" colorType="operator" onClick={onReset} />
+            <Button name="-" colorType="operator" onClick={onOperatorClick} />
           </div>
           <div className={styles.buttons_container}>
             <Button name="1" colorType="number" onClick={onNumberClick} />
             <Button name="2" colorType="number" onClick={onNumberClick} />
             <Button name="3" colorType="number" onClick={onNumberClick} />
-            <Button name="+" colorType="operator" onClick={onReset} />
+            <Button name="+" colorType="operator" onClick={onOperatorClick} />
           </div>
           <div className={styles.buttons_container}>
-            <Button name="(" colorType="operator" onClick={onReset} />
+            <Button name="(" colorType="operator" onClick={onOperatorClick} />
             <Button name="0" colorType="number" onClick={onNumberClick} />
-            <Button name=")" colorType="operator" onClick={onReset} />
-            <Button name="=" colorType="operator" onClick={onReset} />
+            <Button name=")" colorType="operator" onClick={onOperatorClick} />
+            <Button name="=" colorType="operator" onClick={onEqualClick} />
           </div>
         </section>
       </div>
